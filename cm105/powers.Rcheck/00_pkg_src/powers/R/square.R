1c3a7b25dedb90d72687e7f875a3e1d074d1caf9
Kate <kate.tyshchenko@yandex.ru>
2018-11-15 09:26:21 -0800

    delete duplicate

--- cm105/powers.Rcheck/00_pkg_src/powers/R/square.R
+++
@@ -1,7 +0,0 @@
-#' @title Square the input
-#' @param x Vector of numerics
-#' @return The vector x, squared.
-#' @export
-square <- function(x) x^2
-# run document() function to process the tags
-# and then install&restart on the build tab
